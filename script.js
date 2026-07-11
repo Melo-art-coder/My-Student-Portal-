90ab2589c0a417d27ef13712bd625d3cce71cfb7
Melo-art-coder <MelodyKamto@gmail.com>
2026-07-11 12:34:33 +0100

    Change comment for redirect action on form submit

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Grab the elements from the HTML
 const form = document.getElementById("studentForm");
 
 form.addEventListener("submit", function(e) {
-    // Stop the page from reloading
     e.preventDefault();
 
-    // After registration, go to the success page
+    // Redirect to success page
     window.location.href = "success.html";
 });
